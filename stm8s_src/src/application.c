baa64143bf90ed330e796810817ffcc64a8f8dcf
reyno123456 <since_2000@126.com>
2017-12-21 19:26:49 +0800

    milestone: very fast

--- stm8s_src/src/application.c
+++ stm8s_src/src/application.c
@@ -111,8 +111,8 @@ static void bldc_close_loop(void)
 			g_values.ms_cnt++;
 			g_pwm_on_duty++;
 
-			if (g_pwm_on_duty > 100){
-				g_pwm_on_duty = 100;
+			if (g_pwm_on_duty > 250){
+				g_pwm_on_duty = 250;
 			}
 		}
 		
@@ -316,13 +316,13 @@ void timer2_service_close_loop(void)
 				{
 					phase_count_us = g_values.commutation_cnt;
 					phase_count_us =  phase_count_us*2*10;
-					if (phase_count_us > g_values.phase_60degree_cnt)
+					if (phase_count_us > g_values.phase_60degree_cnt + 10)
 					{
-						g_values.phase_60degree_cnt += 10;
+						g_values.phase_60degree_cnt += 1;
 					}
-					else if(phase_count_us < g_values.phase_60degree_cnt)
+					else if(phase_count_us < g_values.phase_60degree_cnt - 10)
 					{
-						g_values.phase_60degree_cnt -= 10;
+						g_values.phase_60degree_cnt -= 1;
 					}
 					i = 0;
 					g_flags.commutation_enable = 0;
